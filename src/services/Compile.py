a3ce8c56f74e0a7f3695de4e15df20c9af115a09
sobraniebluee <dimf29@gmail.com>
2022-11-12 03:28:36 +0100

    "Add compile python3"

--- src/services/Compile.py
+++ src/services/Compile.py
@@ -5,7 +5,6 @@ import subprocess
 from src.config import CompilerConfig, Config
 from src.http_error import NotFoundHttpError, ApiHttpError, ServerHttpError
 from src.models.Code import CodeModel
-from src.services.FileCode import FileCodeService, FileCodeError
 
 
 class CompileService:
@@ -14,16 +13,21 @@ class CompileService:
         code: CodeModel = CodeModel.query.filter(CodeModel.id == id_file).first()
         if not code:
             raise NotFoundHttpError
-        # if code.id_user != id_user:
-        #     raise ApiHttpError(message="You have not permissions to run this code!", status_code=403)
-        # if code.ext not in CompilerConfig.AVAILABLE_COMPILES:
-        #     raise ServerHttpError
+        if code.id_user != id_user:
+            raise ApiHttpError(message="You have not permissions to run this code!", status_code=403)
+        if code.ext not in CompilerConfig.AVAILABLE_COMPILES:
+            raise ServerHttpError
+
         filepath = os.path.join(code.filepath)
+
         try:
             if code.ext == "js":
                 return Compiler.javascript(code.filepath)
             elif code.ext == "ts":
                 return Compiler.typescript(id_code=code.id, filepath=code.filepath, filename=code.title)
+            elif code.ext == "py":
+                return Compiler.python(filepath=code.filepath, filename=code.title)
+
         except BaseCompilerError as e:
             print("Compiler error:", e.message)
             raise ServerHttpError
@@ -105,7 +109,8 @@ class Compiler:
         output_js_filepath = f"{Config.STORAGE_PATH}/{id_code}.js"
 
         output, error = CmdProcess.run(io_reader=True, command=[CompilerConfig.TYPESCRIPT_CMD, "--module", "none", "--pretty", "false", "--strict", "--noEmitOnError", "--outFile", output_js_filepath, filepath])
-
+        # If output approach it is mean typescript generate error,
+        # Check each line for filepath and replace to filename
         if output is not None and not os.path.exists(output_js_filepath):
             lines = output.readlines()
             output_utf8 = ""
@@ -127,6 +132,27 @@ class Compiler:
                                   version_compiler=CompilerConfig.TYPESCRIPT_VERSION)
 
     @classmethod
-    def python(cls, filepath):
-        pass
+    def python(cls, filepath, filename) -> OutputCompiler:
+        relative_filepath = os.path.relpath(filepath)
+        abs_filepath = os.path.abspath(filepath)
+
+        output, error = CmdProcess.run(io_reader=True, command=[CompilerConfig.PYTHON_CMD, filepath])
+
+        if error:
+            lines = error.readlines()
+            output_utf8 = ""
+            for line in lines:
+                output_utf8 += line.decode("utf-8").replace(relative_filepath, filename).replace(abs_filepath, filename)
+
+            return OutputCompiler(value=output_utf8,
+                                  is_error=True,
+                                  version_compiler=CompilerConfig.PYTHON_VERSION)
+
+        if output:
+            output_utf8 = output.read(CompilerConfig.MAX_OUTPUT_LENGTH).decode('utf-8')
+            if output_utf8[-1] != "\n":
+                output_utf8 = output_utf8 + "...\n"
+            return OutputCompiler(value=output_utf8,
+                                  is_error=False,
+                                  version_compiler=CompilerConfig.PYTHON_VERSION)
 

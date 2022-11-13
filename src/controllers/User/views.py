0839a23467666cb8e670440f0e6acc5209c1e228
sobraniebluee <dimf29@gmail.com>
2022-11-13 06:29:25 +0100

    "Add compile php and refactor compile service"

--- src/controllers/User/views.py
+++ src/controllers/User/views.py
@@ -3,7 +3,6 @@ from flask import Blueprint, request, make_response, jsonify
 from src.middlewares.auth_required import auth_required
 from src.schemas.User import UserResponse
 from src.services.User import UserService
-from flask_apispec import use_kwargs, marshal_with
 from src.middlewares.auth_required import UserIdentify
 
 user = Blueprint('user', __name__)
@@ -21,15 +20,13 @@ def auth(identify: UserIdentify):
         response.set_cookie('token', 'Bearer ' + data.tokens.access_token)
         return response
     else:
-         data, status_code = UserService.get_one(identify.id_user)
-         if status_code != 200:
+        data, status_code = UserService.get_one(identify.id_user)
+        if status_code != 200:
             return jsonify(data), status_code
-         return make_response(schema.dump(data), status_code)
+        return make_response(schema.dump(data), status_code)
 
 
 @user.route("/logout", methods=['DELETE'])
 @auth_required(request=request)
 def logout(identify: UserIdentify):
     return UserService.logout(id_user=identify.id_user)
-
-
